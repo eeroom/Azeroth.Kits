b8a4c05128db9a8196fbd89a71f2f9a47eabd7a0
eeroom <eedroom@163.com>
2018-04-04 16:55:48 +0800

    演示c#里面的闭包

---
+++ T41/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T41
+{
+    class Program
+    {
+        /// <summary>
+        /// 演示c#里面的闭包
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            List<Func<int>> handler = GetHandlers();
+            handler.ForEach(x=>System.Console.WriteLine(x().ToString()));
+            Console.ReadKey();
+        }
+
+        private static List<Func<int>> GetHandlers()
+        {
+            List<Func<int>> lst = new List<Func<int>>();
+            for (int i = 0; i < 3; i++)
+            {
+                lst.Add(()=>i);
+            }
+            return lst;
+        }
+    }
+}
