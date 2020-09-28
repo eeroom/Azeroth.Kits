fd5adbe3c43446e7303e0ddb72f4b6eb5d20207c
邓常青 <eedroom@163.com>
2020-09-28 22:39:04 +0800

    Pinvoke的demo

---
+++ NativeFunctionInterop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NativeFunctionInterop
+{
+    public delegate int Function(int v1, int v2);
+
+    public class Program
+    {
+        [System.Runtime.InteropServices.DllImport("NativeFunction.dll")]
+        public static extern int Add(int v1, int v2);
+
+        //P/Invoke中不能使用泛型的委托，222所以定义一个与原方法中callback函数指针方法签名一致的委托
+        [System.Runtime.InteropServices.DllImport("NativeFunction.dll")]
+        public static extern int Handler(IntPtr str, Function callback);
+
+        static void Main(string[] args)
+        {
+            int rst = Add(1, 2);//
+            IntPtr str = System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi("hello world");
+            int rst2 = Handler(str, new Function(RT));
+            Console.ReadKey();
+        }
+
+        static int RT(int v1, int v2)
+        {
+            return v1 * 2 + v2 * 3;
+        }
+    }
+}
