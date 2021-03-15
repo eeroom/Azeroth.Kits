02c2e6de7aa56be1369b8c438fd73e09cff70ebc
邓常青 <eedroom@163.com>
2021-03-15 22:13:17 +0800

    更新命名为Azeroth.Core

--- Azeroth.Core.InteropServices/Properties/AssemblyInfo.cs
+++ Azeroth.Core.InteropServices/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Fw.Native.Interop")]
+[assembly: AssemblyTitle("Azeroth.Core.InteropServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fw.Native.Interop")]
+[assembly: AssemblyProduct("Azeroth.Core.InteropServices")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
