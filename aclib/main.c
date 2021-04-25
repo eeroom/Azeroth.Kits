85f6b91725fddd701a50e0fde63d1d5771d6ab4d
邓常青 <eedroom@163.com>
2021-04-25 22:03:18 +0800

    优化命名

--- aclib/main.c
+++ aclib/main.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
-typedef int (Function)(int v1,int v2);
+typedef int (TransferCallback)(int v1,int v2);
 
 int Add(int v1,int v2)
 {
 	return (v1+v2)*100;
 }
 
-__declspec(dllexport) int Handler(char* str,Function* callback)
+__declspec(dllexport) int Handler(char* str, TransferCallback* callback)
 {
 	printf("%s \r\n",str);
 	return callback(12,22);
