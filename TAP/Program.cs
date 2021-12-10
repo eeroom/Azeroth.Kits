33938ba9e23a796ba5ea30c08ff0f8ca2aa05177
eeroom <eedroom@163.com>
2021-12-10 16:40:26 +0800

    基于task的异步编程模型

--- TAP/Program.cs
+++ TAP/Program.cs
@@ -7,85 +7,28 @@ using System.Threading.Tasks;
 namespace TAP {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("主线程开始");
-            var lst = System.Linq.Enumerable.Range(1, 1000);
-            foreach (var item in lst) {
-                var rt2=  Method1(item);
-                //Method2(item);
-                //Method3(item);
-            }
-            Console.WriteLine("主线程跑完");
+            Handler1(11);
+            Console.WriteLine("hello world");
             Console.ReadLine();
+
         }
 
-        /// <summary>
-        /// 效果：1000个任务差不多同时执行完，每个任务中途都等待了5s
-        ///不会阻塞当前的调用线程，会让其回到线程池执行其他任务，
-        ///代码本身这里任然阻塞，等待后续Task体系安排线程执行await下一行的代码
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        private static async Task<int> Method1(int x ) {
-            int fid = System.Threading.Thread.CurrentThread.ManagedThreadId;
-            var fss = DateTime.Now.Second;
-            //不会阻塞当前的调用线程，会让其回到线程池执行其他任务，
-            //代码本身这里任然阻塞，等待后续Task体系安排线程执行await下一行的代码
-            await Task.Delay(5 * 1000);
-            Console.WriteLine($"value={x},fId={fid},sId={System.Threading.Thread.CurrentThread.ManagedThreadId},fss={fss},sss={DateTime.Now.Second}");
-            return await Task.FromResult(x);
+        async static Task Handler1(int vv)
+        {
+            Console.WriteLine("进入Handler1");
+            await Task.Delay(2000);
+            Console.WriteLine("执行完Task.Delay(2000)");
+            var value = await Handler2(vv);
+            Console.WriteLine("执行完Handler2");
         }
 
-        /// <summary>
-        /// 效果：1000个任务挨个执行完，每个任务中途没有等待
-        ///Task.Delay()，调用后只是一个task对象，因为async方法被编译器改造后，遇到await会return(异步方法的状态机原理),不会阻塞当前线程
-        ///所以fss和sss的值是一样的
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        private static void Method2(int x) {
-            int fid = System.Threading.Thread.CurrentThread.ManagedThreadId;
-            var fss = DateTime.Now.Second;
-            //Task.Delay()，调用后只是一个task对象，
-            //因为async方法被编译器改造后，遇到await会return(异步方法的状态机原理),不会阻塞当前线程
-            //所以fss和sss的值是一样的
-            Task.Delay(5 * 1000);
-            //var rt = Task.Delay(5 * 1000);
-            Console.WriteLine($"value={x},fId={fid},sId={System.Threading.Thread.CurrentThread.ManagedThreadId},fss={fss},sss={DateTime.Now.Second}");
-        }
-
-        /// <summary>
-        /// 效果：1000个任务挨个执行完，每个任务中途都等待了5s
-        /// 1000个任务分批次（批次大小受线程池大小影响）执行完，每个任务中途都等待了5s
-        ///阻塞当前的调用线程
-        ///代码本身这里任然阻塞
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        private static void Method3(int x) {
-            int fid = System.Threading.Thread.CurrentThread.ManagedThreadId;
-            var fss = DateTime.Now.Second;
-            var rt = Task.Delay(5 * 1000);
-            //阻塞当前的调用线程
-            //代码本身这里任然阻塞
-            rt.Wait();
-            Console.WriteLine($"value={x},fId={fid},sId={System.Threading.Thread.CurrentThread.ManagedThreadId},fss={fss},sss={DateTime.Now.Second}");
-        }
-
-        /// <summary>
-        /// 效果：1000个任务分批次（批次大小受线程池大小影响）执行完，每个任务中途都等待了5s
-        ///阻塞当前的调用线程
-        ///代码本身这里任然阻塞
-        /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        private static void Method4(int x) {
-            int fid = System.Threading.Thread.CurrentThread.ManagedThreadId;
-            var fss = DateTime.Now.Second;
-            var rt = Task.Delay(5 * 1000);
-            //阻塞当前的调用线程
-            //代码本身这里任然阻塞
-            rt.Wait();
-            Console.WriteLine($"value={x},fId={fid},sId={System.Threading.Thread.CurrentThread.ManagedThreadId},fss={fss},sss={DateTime.Now.Second}");
+        static async Task<string> Handler2(int vv)
+        {
+            Console.WriteLine("进入Handler2");
+            await Task.Delay(1000);
+            Console.WriteLine("执行完Task.Delay(1000)");
+            var rt = vv.ToString();
+            return rt;
         }
     }
 }
