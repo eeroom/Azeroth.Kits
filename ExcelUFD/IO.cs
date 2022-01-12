4ac1b1e285c9e23c53fda7ed7ef540b58fe1271c
eeroom <eedroom@163.com>
2022-01-12 17:56:57 +0800

    引入excel-dna

---
+++ ExcelUFD/IO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class IO
+{
+    [ExcelDna.Integration.ExcelFunction(Description = "获取当前时间")]
+    public static string DateTimeNow()
+    {
+        return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+    }
+}
---
+++ ExcelUFD/Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class Text
+{
+    public static string ToLower(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+            return value;
+        return value.ToLower();
+    }
+
+    public static string NewGuid(string ff)
+    {
+        return string.IsNullOrEmpty(ff) ? Guid.NewGuid().ToString() : Guid.NewGuid().ToString(ff);
+    }
+}
