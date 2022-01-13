1ee8b3479b16eee26ee221100758517dd374fabb
eeroom <eedroom@163.com>
2022-01-13 18:53:47 +0800

    增加RTD支持

---
+++ ExcelUFD/IRtdS1Handler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelUFD
+{
+    public interface IRtdS1Handler
+    {
+        object ProcessRequest(Array parameter);
+    }
+}
---
+++ ExcelUFD/RtdContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelUFD
+{
+    public class RtdContext
+    {
+        public int TopicId { get; set; }
+
+        public string Action { get; set; }
+
+        public int Interval { get; set; }
+
+        public object Value { get; set; }
+
+        public Array Arguments { get; set; }
+
+        public bool DeleteFlag { get; set; }
+
+        public System.Threading.Timer MyTimer { set; get; }
+    }
+}
