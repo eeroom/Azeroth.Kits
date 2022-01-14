d5e1389e26fa8da7a938b2ec41a3c3de379525d6
eeroom <eedroom@163.com>
2022-01-14 10:23:13 +0800

    vclibInvoke

--- VclibInvoke/Program.cs
+++ VclibInvoke/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AclibPInvoke {
+namespace VclibInvoke
+{
     public delegate int TransferCallback(int v1, int v2);
 
     public class Program
