565832cb28d5058ba633c32f0a503f5eab61fb59
eeroom <eedroom@163.com>
2022-01-14 11:06:54 +0800

    ReportView

--- ReportView/Program.cs
+++ ReportView/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MicrosoftLocalReportView {
+namespace ReportView
+{
     class Program {
         static void Main(string[] args) {
             Microsoft.Reporting.WinForms.LocalReport report = new Microsoft.Reporting.WinForms.LocalReport();
