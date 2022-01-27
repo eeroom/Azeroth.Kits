ff793ab209e03b1eef8b1edde02040c87a9f3d8d
eeroom <eedroom@163.com>
2022-01-27 15:00:03 +0800

    Encrypt

--- BouncyCastleCrypto/GpgUtil.cs
+++ BouncyCastleCrypto/GpgUtil.cs
@@ -49,8 +49,28 @@ namespace BouncyCastleCrypto
             }
         }
 
-        public static void Encrypt()
+        public static void Encrypt(System.IO.FileInfo inputFile, System.IO.Stream outputStream, Org.BouncyCastle.Bcpg.OpenPgp.PgpPublicKey pubKey, GpgEncryptSignCfg cfg)
         {
+            var sr = new Org.BouncyCastle.Security.SecureRandom();
+            var pgpEncryptedDataGenerator = new Org.BouncyCastle.Bcpg.OpenPgp.PgpEncryptedDataGenerator(cfg.SymmetricKeyAlgorithmTag, cfg.IntegrityProtected, sr);
+            pgpEncryptedDataGenerator.AddMethod(pubKey);
+            var pgpCompressedDataGenerator = new Org.BouncyCastle.Bcpg.OpenPgp.PgpCompressedDataGenerator(cfg.CompressionAlgorithmTag);
+            var pgpLiteralDataGenerator = new Org.BouncyCastle.Bcpg.OpenPgp.PgpLiteralDataGenerator();
+
+            using (var fs = inputFile.OpenRead())
+            using (var outputStreamEncrypted = pgpEncryptedDataGenerator.Open(outputStream, new byte[cfg.BufferSize]))
+            using (var outputStreamEncryptedCompressed = pgpCompressedDataGenerator.Open(outputStreamEncrypted))
+            using (var outputStreamEncryptedCompressedLiteral = pgpLiteralDataGenerator.Open(outputStreamEncryptedCompressed,
+                Org.BouncyCastle.Bcpg.OpenPgp.PgpLiteralData.Binary, inputFile.Name, inputFile.Length, inputFile.LastWriteTime))
+            {
+                int dataLenght = 0;
+                var buffer = new byte[cfg.BufferSize];
+                while ((dataLenght = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    outputStreamEncryptedCompressedLiteral.Write(buffer, 0, dataLenght);
+                }
+
+            }
 
         }
     }
