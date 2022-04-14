b93519a3fe4a570eb4dc4e87ff4fbd9abba1dbd9
eeroom <eedroom@163.com>
2022-04-14 23:23:16 +0800

    AutofacDataProvider

---
+++ TenpowerTalk/AutofacDataProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenpowerTalk
+{
+    public class AutofacDataProvider : System.Windows.Data.ObjectDataProvider
+    {
+       unsafe static AutofacDataProvider()
+        {
+            var methodPtrO = (long*)typeof(System.Windows.Data.ObjectDataProvider)
+                .GetMethod("CreateObjectInstance")
+                .MethodHandle.Value.ToPointer() + 1;
+            var methodPtrA = (long*)typeof(AutofacDataProvider)
+               .GetMethod("CreateObjectInstance")
+               .MethodHandle.Value.ToPointer() + 1;
+            *methodPtrO = *methodPtrA;
+        }
+
+        private object CreateObjectInstance(out Exception e)
+        {
+            e = null;
+            var obj = System.Activator.CreateInstance(this.ObjectType);
+            return obj;
+        }
+    }
+}
