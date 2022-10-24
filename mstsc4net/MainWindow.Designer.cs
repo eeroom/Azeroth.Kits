d80716220e09ec83e695b2d43583040ab6627c3c
eeroom <eedroom@163.com>
2022-10-24 20:41:38 +0800

    mstsc4net

---
+++ mstsc4net/MainWindow.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace mstsc4net
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.axMsRdpClient6NotSafeForScripting1 = new AxMSTSCLib.AxMsRdpClient6NotSafeForScripting();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axMsRdpClient6NotSafeForScripting1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMsRdpClient6NotSafeForScripting1
+            // 
+            this.axMsRdpClient6NotSafeForScripting1.Enabled = true;
+            this.axMsRdpClient6NotSafeForScripting1.Location = new System.Drawing.Point(-1, -1);
+            this.axMsRdpClient6NotSafeForScripting1.Name = "axMsRdpClient6NotSafeForScripting1";
+            this.axMsRdpClient6NotSafeForScripting1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMsRdpClient6NotSafeForScripting1.OcxState")));
+            this.axMsRdpClient6NotSafeForScripting1.Size = new System.Drawing.Size(809, 578);
+            this.axMsRdpClient6NotSafeForScripting1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(840, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "地址";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(875, 16);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(242, 21);
+            this.txtIp.TabIndex = 2;
+            this.txtIp.Text = "192.168.1.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(829, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "用户名";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(875, 76);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(242, 21);
+            this.txtUserName.TabIndex = 2;
+            this.txtUserName.Text = "sa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(840, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "密码";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(875, 103);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(242, 21);
+            this.txtPwd.TabIndex = 2;
+            this.txtPwd.Text = "123";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(875, 524);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(219, 39);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "确定";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(840, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "端口";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(875, 46);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(242, 21);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "3389";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 575);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axMsRdpClient6NotSafeForScripting1);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMsRdpClient6NotSafeForScripting1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsRdpClient6NotSafeForScripting axMsRdpClient6NotSafeForScripting1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ mstsc4net/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mstsc4net
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.axMsRdpClient6NotSafeForScripting1.Height = 600;
+            this.axMsRdpClient6NotSafeForScripting1.Width = 800;
+            this.axMsRdpClient6NotSafeForScripting1.Server = this.txtIp.Text;
+            this.axMsRdpClient6NotSafeForScripting1.AdvancedSettings7.RDPPort = int.Parse(this.txtPort.Text);
+            this.axMsRdpClient6NotSafeForScripting1.UserName = this.txtUserName.Text;
+            this.axMsRdpClient6NotSafeForScripting1.AdvancedSettings7.ClearTextPassword = this.txtPwd.Text;
+            this.axMsRdpClient6NotSafeForScripting1.Domain = string.Empty;
+            this.axMsRdpClient6NotSafeForScripting1.AdvancedSettings7.LoadBalanceInfo = string.Empty;
+            this.axMsRdpClient6NotSafeForScripting1.Connect();
+
+        }
+    }
+}
