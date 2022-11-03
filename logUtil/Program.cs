b29601cfb4b5c81b610f949efd345ce04a26deba
eeroom <eedroom@163.com>
2022-11-03 20:36:53 +0800

    logUtil

---
+++ logUtil/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace logUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+            var logger= log4net.LogManager.GetLogger("abc");
+            logger.Error("hello world");
+            System.Console.ReadLine();
+        }
+    }
+}
